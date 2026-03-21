1aa6ad08ee93fab58865c176fa756b5ad6afda93
Liyan Zhao <return65535@qq.com>
2026-03-21 20:16:13 +0800

    feat: add mod

---
+++ space-logger-mod/src/client/java/com/github/zly2006/sl/client/SpaceLoggerClient.kt
@@ -0,0 +1,9 @@
+package com.github.zly2006.sl.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class SpaceLoggerClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ space-logger-mod/src/main/java/com/github/zly2006/sl/SpaceLogger.kt
@@ -0,0 +1,9 @@
+package com.github.zly2006.sl
+
+import net.fabricmc.api.ModInitializer
+
+class SpaceLogger : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
